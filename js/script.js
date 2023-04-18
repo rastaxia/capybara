7212dbe5fc632e046253871dec8dd6f722c8cf44
Rob <rastaxia.gaming@gmail.com>
2023-04-18 14:11:05 +0200

    added css changed name

--- js/script.js
+++ js/script.js
@@ -1,36 +1,45 @@
-const themeMusic = new Audio('../sounds/theme.mp3');
+const themeMusic = new Audio("../sounds/theme.mp3");
 const voiceActing = document.querySelector("#checker");
 const volume = document.querySelector("#volume");
 
-document.querySelector('.launchBtn').addEventListener('click', function() {
-    this.style.display = 'none';
-    themeMusic.play();
-    themeMusic.loop = true;
-    document.querySelector('.menu').style.display = 'block';
+// launch button
+document.querySelector(".launchBtn").addEventListener("click", function () {
+  this.style.display = "none";
+  themeMusic.play();
+  themeMusic.loop = true;
+  document.querySelector(".menu").style.display = "block";
 });
 
-document.querySelector('.start').addEventListener('click', function() {});
-
-document.querySelector('.options').addEventListener('click', function() {
-    document.querySelector('.menu').style.display = 'none';
-    document.querySelector('.optionsScreen').style.display = 'block';
-    volume.addEventListener('change', function(e) {
-        themeMusic.volume = e.currentTarget.value / 100;
-        document.querySelector('#volumeValue').innerHTML = e.currentTarget.value + '%'; 
-    });
-    voiceActing.addEventListener('change', function(e) {
-        if (e.currentTarget.checked) {
-          const voice =  new Audio('../sounds/voiceActing.mp3');
-            voice.play();
-            themeMusic.pause();
-            voice.onended = function() {
-                themeMusic.play();
-                document.querySelector('.voiceOption').style.display = 'none';
-            }
-        } else {
-            console.log('unchecked');
-        }
-    });
+// start game
+document.querySelector(".start").addEventListener("click", function () {});
+// options menus
+document.querySelector(".options").addEventListener("click", function () {
+  document.querySelector(".menu").style.display = "none";
+  document.querySelector(".optionsScreen").style.display = "block";
+  // volume slider
+  volume.addEventListener("change", function (e) {
+    themeMusic.volume = e.currentTarget.value / 100;
+    document.querySelector("#volumeValue").innerHTML =
+      e.currentTarget.value + "%";
+  });
+  // voice acting
+  voiceActing.addEventListener("change", function (e) {
+    if (e.currentTarget.checked) {
+      const voice = new Audio("../sounds/voiceActing.mp3");
+      voice.play();
+      themeMusic.pause();
+      voice.onended = function () {
+        themeMusic.play();
+        document.querySelector(".voiceOption").style.display = "none";
+      };
+    }
+  });
 });
 
-document.querySelector('.quit').addEventListener('click', function() {});
+// back to menu
+document.querySelector(".back").addEventListener("click", function () {
+  document.querySelector(".optionsScreen").style.display = "none";
+  document.querySelector(".menu").style.display = "block";
+});
+
+document.querySelector(".quit").addEventListener("click", function () {});
